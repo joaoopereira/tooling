d9c24a80ade18e22877a927cd9ded9b98a4fa628
João Pereira <joaopereira@criticalmanufacturing.com>
2023-03-15 11:41:41 +0000

    chore(pwsh): git subrepo clone https://github.com/mikebattista/PowerShell-WSL-Interop.git pwsh/wsl-interop
    subrepo:
      subdir:   "pwsh/wsl-interop"
      merged:   "2c8dc7a"
    upstream:
      origin:   "https://github.com/mikebattista/PowerShell-WSL-Interop.git"
      branch:   "main"
      commit:   "2c8dc7a"
    git-subrepo:
      version:  "0.4.5"
      origin:   "???"
      commit:   "???"

--- pwsh/profile/scripts/wsl-alias.ps1
+++ pwsh/profile/scripts/wsl-alias.ps1
@@ -3,7 +3,7 @@
 if ($IsWindows -and $IsAdmin -and
 	((Get-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux).State -eq "Enabled")) {
 	## CREDITS: https://devblogs.microsoft.com/commandline/integrate-linux-commands-into-windows-with-powershell-and-the-windows-subsystem-for-linux/
-	Install-Module WslInterop
+	Import-Module "$env:TOOLING_REPO/pwsh/wsl-interop/WslInterop.psd1"
 	# import commands
 	Import-WslCommand "apt", "awk", "emacs", "find", "grep", "head", "less", "ls", "man", "sed", "seq", "sudo", "tail", "touch", "vim", "docker", "docker-compose", "date", "rm", "earthly", "openssl"
 
@@ -11,7 +11,7 @@ if ($IsWindows -and $IsAdmin -and
 	# with a custom dns entry defined by environment variable LOCAL_DOMAIN
 	# this will allow to have multiple entries
 	# alternative to this function is wsl2host (https://github.com/shayne/go-wsl2-host)
-	function wsl-refresh-ip {
+	function wsl-ip {
 		$env:LOCAL_DOMAIN = $env:LOCAL_DOMAIN ?? "wsl.local"
 		$wslIpAddr = (wsl hostname -I).Trim()
 		$ip = $wslIpAddr.Split(" ")[0]
