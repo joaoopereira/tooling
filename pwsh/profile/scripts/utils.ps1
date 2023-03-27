ed4c6c4d2e53a79593de0bc5032a264e4a3bc93c
João Pereira <joaopereira@criticalmanufacturing.com>
2023-03-27 15:26:59 +0100

    chore(pwsh): check if can access github before check for updates

--- pwsh/profile/scripts/utils.ps1
+++ pwsh/profile/scripts/utils.ps1
@@ -1,27 +1,30 @@
 # check if a new version of the repo is available comparing the last commits
 function CheckToolingUpdates {
-    # move to tooling repo
-    Set-Location $env:TOOLING_REPO
 
-    # Fetch the latest changes from the origin branch
-    git fetch origin > $null
+    if(CanAccessGithub) {
+        # move to tooling repo
+        Set-Location $env:TOOLING_REPO
 
-    # Get the hash of the last commit on the local branch
-    $localCommitHash = (git log -n 1 --pretty=format:"%h" HEAD)
+        # Fetch the latest changes from the origin branch
+        git fetch origin > $null
 
-    # Get the hash of the last commit on the origin branch
-    $originCommitHash = (git log -n 1 --pretty=format:"%h" origin/main)
+        # Get the hash of the last commit on the local branch
+        $localCommitHash = (git log -n 1 --pretty=format:"%h" HEAD)
 
-    # Compare the commit hashes to determine which is most recent
-    if (git log $localCommitHash..$originCommitHash | Measure-Object | Select-Object -ExpandProperty Count) {
-        Write-Host "A new update is available! Please run " -NoNewline -ForegroundColor DarkYellow;
-        Write-Host -ForegroundColor DarkBlue "tupdate" -NoNewline;
-        Write-Host " to get the latest version." -NoNewline -ForegroundColor DarkYellow;
-        Write-Host
+        # Get the hash of the last commit on the origin branch
+        $originCommitHash = (git log -n 1 --pretty=format:"%h" origin/main)
+
+        # Compare the commit hashes to determine which is most recent
+        if (git log $localCommitHash..$originCommitHash | Measure-Object | Select-Object -ExpandProperty Count) {
+            Write-Host "A new update is available! Please run " -NoNewline -ForegroundColor DarkYellow;
+            Write-Host -ForegroundColor DarkBlue "tupdate" -NoNewline;
+            Write-Host " to get the latest version." -NoNewline -ForegroundColor DarkYellow;
+            Write-Host
+        }
+
+        # move back to source folder
+        Set-Location -
     }
-
-    # move back to source folder
-    Set-Location -
 }
 
 # chocolatey for windows
@@ -39,4 +42,12 @@ function SetWindowsAdmin {
             Write-Host "PowerShell is not running with administrative privileges. Some features may not work." -ForegroundColor Yellow
         }
     }
+}
+
+function CanAccessGithub {
+    $global:ProgressPreference = "SilentlyContinue"
+    $result = (Test-NetConnection github.com -WarningAction SilentlyContinue -ErrorAction SilentlyContinue).PingSucceeded
+    $global:ProgressPreference = "Continue"
+
+    return $result
 }
