2eb7785e47ee9b1dbca11f97d5fc9b856eadbfee
João Pereira <joao.octavio.pereira@gmail.com>
2023-08-17 00:34:22 +0100

    chore(pwsh): fix installation script

--- pwsh/profile/install.ps1
+++ pwsh/profile/install.ps1
@@ -1,13 +1,17 @@
 $git = [bool](Get-Command git -ErrorAction SilentlyContinue)
 if(!$git)
 {
-    Write-Host "Mandatory dependency git is not installed."
-    break
+    Write-Host "git is not installed. Installing..."
+    choco install -y git
 }
 
 git clone https://github.com/joaoopereira/tooling.git ./tooling
 $basePath = "$pwd/tooling/pwsh/profile/base.ps1"
 
+if(!(Test-Path $PROFILE))
+{
+    "" >> $PROFILE
+}
 if(!(Get-Content $PROFILE).Contains(". `"$basePath`""))
 {
     ". `"$basePath`"" >> $PROFILE
