1fb0a30a98ab03f5063a3fe6130accdf83fb5336
João Pereira <joaopereira@criticalmanufacturing.com>
2023-08-17 11:22:18 +0100

    chore(pwsh): force pwsh install

--- pwsh/profile/README.md
+++ pwsh/profile/README.md
@@ -1,9 +1,11 @@
-# requirements
-this poweshell profile, installs by default a package manager:
-- [chocolatey](https://chocolatey.org/) (on windows)
-
 # installation
 in the target folder, run:
 ```powershell
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression (New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/joaoopereira/tooling/main/pwsh/profile/install.ps1")
 ```
+# notes
+this poweshell profile installer, installs by default:
+- [chocolatey](https://chocolatey.org/)
+- [git](https://git-scm.com/)
+- [pwsh](https://learn.microsoft.com/powershell/scripting/overview)
+- [oh-my-posh](https://ohmyposh.dev/)
--- pwsh/profile/install.ps1
+++ pwsh/profile/install.ps1
@@ -1,5 +1,6 @@
 $choco = [bool](Get-Command choco -ErrorAction SilentlyContinue)
 if (!$choco) {
+    Write-Host "chocolatey is not installed. Installing..."
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
@@ -8,18 +9,40 @@ if(!$git)
 {
     Write-Host "git is not installed. Installing..."
     choco install -y git
-    # Update current process PATH environment variable
-    $env:Path = [Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::Machine);
 }
 
-git clone https://github.com/joaoopereira/tooling.git ./tooling
-$basePath = "$pwd/tooling/pwsh/profile/base.ps1"
+$pwsh = [bool](Get-Command pwsh -ErrorAction SilentlyContinue)
+if(!$pwsh)
+{
+    Write-Host "pwsh is not installed. Installing..."
+    choco install -y pwsh
+}
 
-if(!(Test-Path $PROFILE))
+$ohmyposh = [bool](Get-Command oh-my-posh -ErrorAction SilentlyContinue)
+if(!$ohmyposh)
 {
-    "" >> $PROFILE
+    Write-Host "oh-my-posh is not installed. Installing..."
+    choco install -y oh-my-posh
 }
-if(!(Get-Content $PROFILE).Contains(". `"$basePath`""))
-{
-    ". `"$basePath`"" >> $PROFILE
+
+# Update current process PATH environment variable
+$env:Path = [Environment]::GetEnvironmentVariable('Path', [System.EnvironmentVariableTarget]::Machine);
+
+ # configure pwsh
+pwsh -Command {
+
+    git clone https://github.com/joaoopereira/tooling.git ./tooling
+    $basePath = "$pwd/tooling/pwsh/profile/base.ps1"
+
+    if(!(Test-Path $PROFILE))
+    {
+        New-Item -Path $PROFILE -Value "`n" -ItemType File -Force
+    }
+    if(!(Get-Content $PROFILE).Contains(". `"$basePath`""))
+    {
+        ". `"$basePath`"" >> $PROFILE
+    }
 }
+
+# open
+pwsh
