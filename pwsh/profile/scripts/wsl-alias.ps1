12eacb615eaa1166f6b2b79c2139727dafac78aa
João Pereira <joao.octavio.pereira@gmail.com>
2023-08-19 09:06:23 +0100

    chore(pwsh): remove ls from wsl alias

--- pwsh/profile/scripts/wsl-alias.ps1
+++ pwsh/profile/scripts/wsl-alias.ps1
@@ -5,7 +5,7 @@ if ($global:IS_WINDOWS_ADMIN -and
 	## CREDITS: https://devblogs.microsoft.com/commandline/integrate-linux-commands-into-windows-with-powershell-and-the-windows-subsystem-for-linux/
 	Import-Module "$env:TOOLING_REPO/pwsh/profile/plugins/wsl-interop/WslInterop.psm1"
 	# import commands
-	Import-WslCommand "apt", "awk", "emacs", "find", "grep", "head", "less", "ls", "man", "sed", "seq", "sudo", "tail", "touch", "vim", "docker", "docker-compose", "date", "rm", "earthly", "openssl", "make", "wget", "export"
+	Import-WslCommand "apt", "awk", "emacs", "find", "grep", "head", "less", "man", "sed", "seq", "sudo", "tail", "touch", "vim", "docker", "docker-compose", "date", "rm", "earthly", "openssl", "make", "wget", "export"
 
 	# restart wsl
 	function wslr {
