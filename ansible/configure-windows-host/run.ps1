df0c53f3acb60bb1cbafd59cc83d2edd31fac976
João Pereira <joaopereira@criticalmanufacturing.com>
2023-08-22 12:04:39 +0100

    chore(ansible): update ConfigureRemotingForAnsible script url

--- ansible/configure-windows-host/run.ps1
+++ ansible/configure-windows-host/run.ps1
@@ -1,5 +1,5 @@
 #### Inputs
-$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$url = "https://raw.githubusercontent.com/ansible/ansible-documentation/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
 $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 $ErrorActionPreference = "Stop";
 
