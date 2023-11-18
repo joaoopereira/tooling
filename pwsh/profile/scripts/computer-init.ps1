99995898aa6d419032a81a651b67a65b500b3ead
João Pereira <mail@jo-pereira.com>
2023-11-18 16:43:16 +0000

    fix(pwsh): small improvements computer init:
    - add git config inputs
    - remove show supper hidden from explorer setup

--- pwsh/profile/scripts/computer-init.ps1
+++ pwsh/profile/scripts/computer-init.ps1
@@ -1,28 +1,30 @@
-function SetupGit {
+function SetupGit() {
+    [string]$userName = Read-Host -Prompt "Enter the git config global user.name"
+    [string]$userEmail = Read-Host -Prompt "Enter the git config global user.email"
+    git config --global user.name $userName
+    git config --global user.email $userEmail
     git config --global core.editor "code --wait"
 }
 
-function SetupChocoPackages($packageConfig) {
-    if(!$packageConfig -or !(Test-Path $packageConfig)) {
-        [string]$packageConfig = Read-Host -Prompt "Enter the path to the chocolatey packages.config"
-        if(Test-Path $packageConfig) {
-            choco install -y $packageConfig
-        } else {
-            Write-Error "$packageConfig not found!"
-        }
+function SetupChocoPackages() {
+    [string]$packageConfig = Read-Host -Prompt "Enter the path to the chocolatey packages.config"
+    if(Test-Path $packageConfig) {
+        choco install -y $packageConfig
+    } elseif (!$packageConfig) {
+        Write-Warning "No path provided, skipping choco packages installation"
+    } else {
+        Write-Error "$packageConfig not found!"
     }
 }
 
 function DisableWin11ShowMoreOptions {
-    $regPath = 'HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}'
-    Set-ItemProperty $regPath InprocServer32 ''
+    (reg add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve) | Out-Null
 }
 
 function SetupWindowsExplorer {
     $regPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
     Set-ItemProperty $regPath Hidden 1
     Set-ItemProperty $regPath HideFileExt 0
-    Set-ItemProperty $regPath ShowSuperHidden 1
     Stop-Process -processname explorer
 }
 
@@ -30,4 +32,3 @@ SetupGit
 SetupWindowsExplorer
 DisableWin11ShowMoreOptions
 SetupChocoPackages
-
